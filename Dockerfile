47d516beb21b9b9321fdbde315f88b8f65c6e3fb
Ji Kuai <rk427454171@gmail.com>
2024-07-04 02:18:36 +0800

    fix: deployment error due to misconfigured Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM node:latest
+FROM node:22
 
 ADD package.json package-lock.json /app
 WORKDIR /app
 RUN npm ci
 ADD . /app
-RUN npm build
+RUN npm run build
 CMD ["npm", "start"]
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+docker stop hackathon2024-frontend
+docker rm hackathon2024-frontend
+docker image rm hackathon2024-frontend
 docker build -t hackathon2024-frontend .
-docker run -itd --name hackathon2024-frontend -v .:/app -p 127.0.0.1:5010:3000 .
+docker run -d --name hackathon2024-frontend -p 127.0.0.1:5010:3000 hackathon2024-frontend
