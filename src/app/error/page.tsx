aae646abebaf70f145d77e893fa719f4afa51f0e
Ji Kuai <rk427454171@gmail.com>
2024-07-04 17:29:53 +0800

    fix: description miss and use a tag for error page to prevent caching

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,6 +3,6 @@ import PageLink from "@/components/page_link";
 export default function ErrorPage() {
   return (<main className="fullscreen flex justify-center items-center flex-col p-5 gap-6 max-w-[500px] m-auto">
     <h1 className="prose">系統錯誤！請稍後再試</h1>
-    <PageLink href="/" text="重新整理" />
+    <a className="btn btn-primary" href="/">重新整理</a>
   </main>);
 }
